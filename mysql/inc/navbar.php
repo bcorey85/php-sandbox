94ac0319aed8c4844e4b1dd49fd779ef91969a50
Brandon Corey <bcorey85@gmail.com>
2020-11-01 18:38:17 -0600

    add create, update, delete function to blog demo

--- mysql/inc/navbar.php
+++ mysql/inc/navbar.php
@@ -1,12 +1,15 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">PHPblog</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?php echo ROOT_URL ?>/">Home <span class="sr-only">(current)</span></a>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo ROOT_URL ?>/">Home</a>
+      </li>
+      <li>
+        <a class="nav-link" href="<?php echo ROOT_URL ?>/addpost.php">Add Post</a>
       </li>
     </ul>
   </div>
